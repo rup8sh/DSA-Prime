bbf262858a3084dd602b89b4f704faa69974cf9b
Kumar-R <rup8shkumar@gmail.com>
2024-06-12 17:36:24 +0530

    Notes

---
+++ OOP/notes.py
@@ -0,0 +1,53 @@
+'''bool       immutable
+int        immutable
+float      immutable
+list
+tuple      immutable
+str        immutable
+set
+frozenset  immutable
+dict
+'''
+print(int(-5.78))
+print(float(-5.798231234567891))
+
+'''
+The list, tuple, and str classes are sequence types
+in Python, representing a collection of values in which
+the order is significant.
+'''
+
+'''
+List = Array
+Tuple = Immutable Array
+String = Immutable String
+'''
+str = 'Don\'t worry'
+newstr = 'Don\'t \n Worry'
+tabstr = 'Don\'t \t Worry'
+print(str, newstr, tabstr)
+
+'''
+Python's set class represents the mathematical notion
+of a set, namely a collection of elements, without
+duplicates, and without an inherent order to those
+elements.
+'''
+listarray = []
+tuplearray = ()
+settype = {}
+
+dictarray = [('a','b'), ('c','d'), ('e','f')]
+
+print(listarray)
+
+print(bool(1 and 1))
+
+print(bool(1 and 0))
+
+print(not 1)
+
+print(not 0)
+
+print((1 or 2))
+
